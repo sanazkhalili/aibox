dd893ba9cea6c1ca35b7a8d1fe927102dd021e82
Sanaz Khalili <74542890+sanazkhalili@users.noreply.github.com>
2022-07-09 15:20:16 +0430

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,50 @@
+FROM nvidia/cuda:11.1.1-cudnn8-devel-ubuntu18.04
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update
+RUN apt-get install -y libgl1
+RUN apt-get install -y cmake
+
+RUN apt install -y tcl
+RUN apt-get install -y g++ && \
+    apt-get install -y build-essential
+
+# Update Python
+RUN apt-get install -y python3.7
+
+
+
+
+RUN update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+
+RUN update-alternatives --config python3
+
+
+RUN apt install -y python3-pip
+
+
+
+RUN python3.7 -m pip install --upgrade pip
+
+
+
+
+WORKDIR /srv
+ADD ./requirements.txt /srv/requirements.txt
+
+
+
+RUN pip3 install -r requirements.txt
+
+ADD . /srv/
+
+
+
+RUN apt install -y libpython3.7-dev
+
+
+WORKDIR /app
+COPY . /app
+
+
+CMD python3.7 mycode.py
+
---
+++ objectDetectionYolo.py
@@ -0,0 +1,452 @@
+import os
+
+import scipy.io
+import scipy.misc
+import numpy as np
+import pandas as pd
+import struct
+import cv2
+from numpy import expand_dims
+import tensorflow as tf
+from skimage.transform import resize
+from keras import backend as K
+from keras.layers import Input, Lambda, Conv2D, BatchNormalization, LeakyReLU, ZeroPadding2D, UpSampling2D
+from keras.layers import Conv2D, MaxPool2D,Dropout, Dense,Lambda, Input, concatenate, GlobalAveragePooling2D, AveragePooling2D,Flatten
+
+from keras.models import load_model, Model
+from keras.layers.merge import add, concatenate
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from matplotlib.patches import Rectangle
+
+
+def _conv_block(inp, convs, skip=True):
+    x = inp
+    count = 0
+    
+    for conv in convs:
+        if count == (len(convs) - 2) and skip:
+            skip_connection = x
+        count += 1
+        
+        if conv['stride'] > 1: x = ZeroPadding2D(((1,0),(1,0)))(x) # peculiar padding as darknet prefer left and top
+        x = Conv2D(conv['filter'], 
+                   conv['kernel'], 
+                   strides=conv['stride'], 
+                   padding='valid' if conv['stride'] > 1 else 'same', # peculiar padding as darknet prefer left and top
+                   name='conv_' + str(conv['layer_idx']), 
+                   use_bias=False if conv['bnorm'] else True)(x)
+        if conv['bnorm']: x = BatchNormalization(epsilon=0.001, name='bnorm_' + str(conv['layer_idx']))(x)
+        if conv['leaky']: x = LeakyReLU(alpha=0.1, name='leaky_' + str(conv['layer_idx']))(x)
+
+    return add([skip_connection, x]) if skip else x
+def make_yolov3_model():
+    input_image = Input(shape=(None, None, 3))
+
+    # Layer  0 => 4
+    x = _conv_block(input_image, [{'filter': 32, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 0},
+                                  {'filter': 64, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 1},
+                                  {'filter': 32, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 2},
+                                  {'filter': 64, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 3}])
+
+    # Layer  5 => 8
+    x = _conv_block(x, [{'filter': 128, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 5},
+                        {'filter':  64, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 6},
+                        {'filter': 128, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 7}])
+
+    # Layer  9 => 11
+    x = _conv_block(x, [{'filter':  64, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 9},
+                        {'filter': 128, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 10}])
+
+    # Layer 12 => 15
+    x = _conv_block(x, [{'filter': 256, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 12},
+                        {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 13},
+                        {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 14}])
+
+    # Layer 16 => 36
+    for i in range(7):
+        x = _conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 16+i*3},
+                            {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 17+i*3}])
+        
+    skip_36 = x
+        
+    # Layer 37 => 40
+    x = _conv_block(x, [{'filter': 512, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 37},
+                        {'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 38},
+                        {'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 39}])
+
+    # Layer 41 => 61
+    for i in range(7):
+        x = _conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 41+i*3},
+                            {'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 42+i*3}])
+        
+    skip_61 = x
+        
+    # Layer 62 => 65
+    x = _conv_block(x, [{'filter': 1024, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 62},
+                        {'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 63},
+                        {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 64}])
+
+    # Layer 66 => 74
+    for i in range(3):
+        x = _conv_block(x, [{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 66+i*3},
+                            {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 67+i*3}])
+        
+    # Layer 75 => 79
+    x = _conv_block(x, [{'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 75},
+                        {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 76},
+                        {'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 77},
+                        {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 78},
+                        {'filter':  512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 79}], skip=False)
+
+    # Layer 80 => 82
+    yolo_82 = _conv_block(x, [{'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 80},
+                              {'filter':  255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 81}], skip=False)
+
+    # Layer 83 => 86
+    x = _conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 84}], skip=False)
+    x = UpSampling2D(2)(x)
+    x = concatenate([x, skip_61])
+
+    # Layer 87 => 91
+    x = _conv_block(x, [{'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 87},
+                        {'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 88},
+                        {'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 89},
+                        {'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 90},
+                        {'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 91}], skip=False)
+
+    # Layer 92 => 94
+    yolo_94 = _conv_block(x, [{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 92},
+                              {'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 93}], skip=False)
+
+    # Layer 95 => 98
+    x = _conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True,   'layer_idx': 96}], skip=False)
+    x = UpSampling2D(2)(x)
+    x = concatenate([x, skip_36])
+
+    # Layer 99 => 106
+    yolo_106 = _conv_block(x, [{'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 99},
+                               {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 100},
+                               {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 101},
+                               {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 102},
+                               {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 103},
+                               {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True,  'leaky': True,  'layer_idx': 104},
+                               {'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False, 'layer_idx': 105}], skip=False)
+
+    model = Model(input_image, [yolo_82, yolo_94, yolo_106])    
+    return model
+def box_iou(b1, b2):
+
+    # Expand dim to apply broadcasting.
+    b1 = K.expand_dims(b1, -2)
+    b1_xy = b1[..., :2]
+    b1_wh = b1[..., 2:4]
+    b1_wh_half = b1_wh / 2.0
+    b1_mins = b1_xy - b1_wh_half
+    b1_maxes = b1_xy + b1_wh_half
+
+    # Expand dim to apply broadcasting.
+    b2 = K.expand_dims(b2, 0)
+    b2_xy = b2[..., :2]
+    b2_wh = b2[..., 2:4]
+    b2_wh_half = b2_wh / 2.0
+    b2_mins = b2_xy - b2_wh_half
+    b2_maxes = b2_xy + b2_wh_half
+
+    intersect_mins = K.maximum(b1_mins, b2_mins)
+    intersect_maxes = K.minimum(b1_maxes, b2_maxes)
+    intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.0)
+    intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+    b1_area = b1_wh[..., 0] * b1_wh[..., 1]
+    b2_area = b2_wh[..., 0] * b2_wh[..., 1]
+    iou = intersect_area / (b1_area + b2_area - intersect_area)
+
+    return iou
+    
+def load_image_pixels(filename, shape):
+    # load the image to get its shape
+    image = load_img(filename)
+    width, height = image.size
+    # load the image with the required size
+    image = load_img(filename, target_size=shape)
+    # convert to numpy array
+    image = img_to_array(image)
+    # scale pixel values to [0, 1]
+    image = image.astype('float32')
+    image /= 255.0
+    # add a dimension so that we have one sample
+    image = expand_dims(image, 0)
+    return image, width, height
+
+
+class BoundBox:
+    def __init__(self, xmin, ymin, xmax, ymax, objness = None, classes = None):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+        
+        self.objness = objness
+        self.classes = classes
+
+        self.label = -1
+        self.score = -1
+
+    def get_label(self):
+        if self.label == -1:
+            self.label = np.argmax(self.classes)
+        
+        return self.label
+    
+    def get_score(self):
+        if self.score == -1:
+            self.score = self.classes[self.get_label()]
+            
+        return self.score
+
+def _sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+def _interval_overlap(interval_a, interval_b):
+    x1, x2 = interval_a
+    x3, x4 = interval_b
+
+    if x3 < x1:
+        if x4 < x1:
+            return 0
+        else:
+            return min(x2,x4) - x1
+    else:
+        if x2 < x3:
+             return 0
+        else:
+            return min(x2,x4) - x3 
+
+def bbox_iou(box1, box2):
+    intersect_w = _interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+    intersect_h = _interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])
+    
+    intersect = intersect_w * intersect_h
+
+    w1, h1 = box1.xmax-box1.xmin, box1.ymax-box1.ymin
+    w2, h2 = box2.xmax-box2.xmin, box2.ymax-box2.ymin
+    
+    union = w1*h1 + w2*h2 - intersect
+    
+    return float(intersect) / union
+
+def do_nms(boxes, nms_thresh):
+    if len(boxes) > 0:
+        nb_class = len(boxes[0].classes)
+    else:
+        return
+        
+    for c in range(nb_class):
+        sorted_indices = np.argsort([-box.classes[c] for box in boxes])
+
+        for i in range(len(sorted_indices)):
+            index_i = sorted_indices[i]
+
+            if boxes[index_i].classes[c] == 0: continue
+
+            for j in range(i+1, len(sorted_indices)):
+                index_j = sorted_indices[j]
+
+                if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_thresh:
+                    boxes[index_j].classes[c] = 0
+
+
+def decode_netout(netout, anchors, obj_thresh,  net_h, net_w):
+    grid_h, grid_w = netout.shape[:2]
+    nb_box = 3
+    netout = netout.reshape((grid_h, grid_w, nb_box, -1))
+    nb_class = netout.shape[-1] - 5
+
+    boxes = []
+
+    netout[..., :2]  = _sigmoid(netout[..., :2])
+    netout[..., 4:]  = _sigmoid(netout[..., 4:])
+    netout[..., 5:]  = netout[..., 4][..., np.newaxis] * netout[..., 5:]
+    netout[..., 5:] *= netout[..., 5:] > obj_thresh
+
+    for i in range(grid_h*grid_w):
+        row = i / grid_w
+        col = i % grid_w
+        
+        for b in range(nb_box):
+            # 4th element is objectness score
+            objectness = netout[int(row)][int(col)][b][4]
+         
+            
+            if(objectness.all() <= obj_thresh): continue
+            
+            # first 4 elements are x, y, w, and h
+            x, y, w, h = netout[int(row)][int(col)][b][:4]
+
+            x = (col + x) / grid_w # center position, unit: image width
+            y = (row + y) / grid_h # center position, unit: image height
+            w = anchors[2 * b + 0] * np.exp(w) / net_w # unit: image width
+            h = anchors[2 * b + 1] * np.exp(h) / net_h # unit: image height  
+            
+            # last elements are class probabilities
+            classes = netout[int(row)][col][b][5:]
+            
+            box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, objectness, classes)
+          
+
+            boxes.append(box)
+
+    return boxes
+
+
+def correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w):
+
+    for i in range(len(boxes)):
+        
+        boxes[i].xmin = int(boxes[i].xmin * image_w)
+        boxes[i].xmax = int(boxes[i].xmax * image_w)
+        boxes[i].ymin = int(boxes[i].ymin * image_h)
+        boxes[i].ymax = int(boxes[i].ymax * image_h)
+
+
+def draw_boxes(filename, v_boxes, v_labels, v_scores):
+    # load the image
+    data = plt.imread(filename)
+    # plot the image
+    plt.imshow(data)
+    # get the context for drawing boxes
+    ax = plt.gca()
+    # plot each box
+    for i in range(len(v_boxes)):
+        box = v_boxes[i]
+        # get coordinates
+        y1, x1, y2, x2 = box.ymin, box.xmin, box.ymax, box.xmax
+        # calculate width and height of the box
+        width, height = x2 - x1, y2 - y1
+        # create the shape
+        rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+        # draw the box
+        ax.add_patch(rect)
+        # draw text and score in top left corner
+        label = "%s (%.3f)" % (v_labels[i], v_scores[i])
+        plt.text(x1, y1,v_labels[i], color='red')
+    # show the plot
+    plt.show()
+
+# get all of the results above a threshold
+# takes the list of boxes, known labels, 
+#and our classification threshold as arguments and returns parallel lists of boxes, labels, and scores.
+def get_boxes(boxes, labels, thresh):
+    v_boxes, v_labels, v_scores = list(), list(), list()
+    # enumerate all boxes
+    for box in boxes:
+        # enumerate all possible labels
+        for i in range(len(labels)):
+            # check if the threshold for this label is high enough
+            if box.classes[i] > thresh:
+                v_boxes.append(box)
+                v_labels.append(labels[i])
+                v_scores.append(box.classes[i]*100)
+                # don't break, many labels may trigger for one box
+    return v_boxes, v_labels, v_scores
+
+
+
+
+from numpy import expand_dims
+from matplotlib.patches import Rectangle
+
+def main(path_weight, photo_filename, obj_thresh=0.3, nms_thresh=0.3):
+
+      input_w, input_h = 416, 416
+
+      net_h, net_w = 416, 416
+
+      anchors = [[116, 90, 156, 198, 373, 326], [30, 61, 62, 45, 59, 119], [10, 13, 16, 30, 33, 23]]
+      labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", \
+                "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", \
+                "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", \
+                "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", \
+                "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", \
+                "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", \
+                "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", \
+                "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", \
+                "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", \
+                "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+     
+      yolov3 = make_yolov3_model()
+
+      yolov3.load_weights(path_weight)
+
+
+      # load and prepare image
+      image, image_w, image_h = load_image_pixels(photo_filename, (net_w, net_w))
+
+
+      # make prediction
+      yolos = yolov3.predict(image)
+
+      # define the probability threshold for detected objects
+      class_threshold = 0.1
+
+      boxes = list()
+
+      for i in range(len(yolos)):
+              # decode the output of the network
+          boxes += decode_netout(yolos[i][0], anchors[i], obj_thresh,  net_h, net_w)
+
+          # correct the sizes of the bounding boxes
+      correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w)
+
+      # suppress non-maximal boxes
+      do_nms(boxes, nms_thresh)
+
+      # get the details of the detected objects
+      v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
+      box_result = {}
+      result = []
+
+      for i in range(len(v_scores)):
+          box_result = {}
+          box_result['minx'] = v_boxes[i].xmin
+          box_result['maxx'] = v_boxes[i].xmax
+          box_result['miny'] = v_boxes[i].ymin
+          box_result['maxy'] = v_boxes[i].ymax
+          
+          box_result['score'] = v_scores[i]
+          box_result['class'] = v_labels[i]
+          result.append(box_result)
+      print("fff", len(result))
+      return result
+
+
+
+
+from flask import Flask,request,send_file
+from flask import jsonify
+
+app = Flask(__name__)
+
+output_list=[]
+
+@app.route('http://0.0.0.0:2000',methods=['POST'])
+def start_program():
+    f = request.files['image']
+    
+    file_path = f.filename
+    f.save(file_path)
+
+    obj_thresh = request.form.get('obj_thresh', type=float)
+
+    output_list = main('./model.h5',file_path, obj_thresh)
+    return jsonify(output_list)
+
+app.run()
+
+
+
+
+    
